383c8374b5473afa408d6e443d9ea1c8763a3a47
Timothy Jones <palantar@gmail.com>
2015-12-12 22:37:09 -0800

    Addition of Materialize Basic layout of voter screen 4 complete.

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,22 +12,6 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 
 /*
- * Directive
- * XLarge is a simple directive to show how one is made
- */
-@Directive({
-  selector: '[x-large]' // using [ ] means selecting attributes
-})
-export class XLarge {
-  constructor(element: ElementRef) {
-    // simple DOM manipulation to set font size to x-large
-    // `nativeElement` is the direct reference to the DOM element
-    element.nativeElement.style.fontSize = 'x-large';
-  }
-}
-
-
-/*
  * App Component
  * Top Level Component
  */
@@ -38,7 +22,7 @@ export class XLarge {
   selector: 'app', // <app></app>
   // We need to tell Angular's compiler which directives are in our template.
   // Doing so will allow Angular to attach our behavior to an element
-  directives: [ ROUTER_DIRECTIVES, XLarge ],
+  directives: [ ROUTER_DIRECTIVES ],
   // Our list of styles in our component. We may add more to compose many styles together
   styles: [`
     .title {
@@ -50,29 +34,75 @@ export class XLarge {
   `],
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
   template: `
-  <header>
-    <h1 class="title">Hello {{ title }}</h1>
-  </header>
+  <nav class="top-nav">
+    <div class="container">
+      <div class="nav-wrapper">Lunchtime Voter</div>
 
-  <main>
-    Your Content Here
-    <div>
+    </div>
+  </nav>
 
-      <input type="text" [value]="title" (input)="title = $event.target.value" autofocus>
-      <!--
-        Rather than wiring up two-way data-binding ourselves
-        we can use Angular's [(ng-model)] syntax
-        <input type="text" [(ng-model)]="title">
-      -->
+  <div class="container">
+    <br/>
+    <div>Add your suggestion or vote on other people's suggestions until 11:30.  At 11:30, voting stops and a spot is chosen!</div>
+    <h4>Add a Lunch Option</h4>
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input placeholder="Thai Siam" id="title" type="text" class="validate">
+          <label for="title">Title</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input placeholder="It's awesome and I love it." id="description" type="text" class="validate">
+          <label for="description">Description / Selling It</label>
+        </div>
+      </div>
+    </form>
+
+    <div class="right-align">
+      <a class="waves-effect waves-light btn"><i class="material-icons left">library_add</i>add</a>
     </div>
 
-    <pre>this.title = {{ title | json }}</pre>
-    <pre>this.data = {{ data | json }}</pre>
+    <h4>Rate other people's suggestions</h4>
+    <table class="bordered">
+        <thead>
+          <tr>
+              <th data-field="id">Place</th>
+              <th data-field="name">Description</th>
+              <th data-field="price">My Rating</th>
+          </tr>
+        </thead>
 
-  </main>
+        <tbody>
+          <tr>
+            <td>Thai Siam</td>
+            <td>The best Thai Food In the World</td>
+            <td>
+              <i style="color: #f5f5f5" class="material-icons dp48">star</i>
+              <i style="color: #f5f5f5" class="material-icons dp48">star</i>
+              <i style="color: #f5f5f5" class="material-icons dp48">star</i>
+              <i style="color: #f5f5f5" class="material-icons dp48">star</i>
+            </td>
+          </tr>
+          <tr>
+            <td>Not Thai Siam</td>
+            <td>Bad Thai Food</td>
+            <td>
+              <i style="color: #f5f5f5" class="material-icons dp48">star</i>
+              <i style="color: #f5f5f5" class="material-icons dp48">star</i>
+              <i style="color: #f5f5f5" class="material-icons dp48">star</i>
+              <i style="color: #f5f5f5" class="material-icons dp48">star</i>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
 
-  <footer x-large>
-    WebPack Angular 2 Starter by <a href="https://twitter.com/AngularClass">@AngularClass</a>
+  <footer class="page-footer white-text">
+    <div class="container">
+      LunchtimeVoter is an open source project, fork your own at <a class="white-text" style="text-decoration: underline" href="https://github.com/OverclockedTim/lunchtime-voter">Github</a>
+    </div>
   </footer>
   `
 })
@@ -81,63 +111,8 @@ export class App {
   title: string;
   data: Array<any> = []; // default data
   // TypeScript public modifiers
-  constructor(public http: Http) {
-    this.title = 'Angular 2';
+  constructor() {
+    this.title = 'Thai Siam';
   }
-
-  ngOnInit() {
-    // Our API
-    // Before you start the app, run these commands in another process:
-    //
-    // - npm run express-install
-    // - npm run express
-    //
-    // This will start a process that will listen for requests on port 3001
-
-    const BASE_URL = 'http://localhost:3001';
-    const TODO_API_URL = '/api/todos';
-    const JSON_HEADERS = new Headers();
-
-    JSON_HEADERS.append('Accept', 'application/json');
-    JSON_HEADERS.append('Content-Type', 'application/json');
-
-    this.http
-      .get(BASE_URL + TODO_API_URL, {
-        headers: JSON_HEADERS
-      })
-      .subscribe(
-        // onNext callback
-        data => this.serverData(data),
-        // onError callback
-        err  => this.errorMessage(err),
-        // onComplete callback
-        ()   => console.log('complete')
-      );//end http
-
-  }
-
-  serverData(data) {
-    console.log('data', data);
-    this.data = data;
-  }//serverData
-
-  errorMessage(err) {
-    console.info(`${'\n'
-      } // You must run these commands in another process for the Http API to work  ${'\n'
-      } npm run express-install ${'\n'
-      } npm run express
-    `);
-  }//errorMessage
-
 }
 
-
-
-/*
- * Please review the examples/ folder for more angular app examples
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * you can change the `entry` in webpack.config to quickly view the examples
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- * or via chat on Gitter at https://gitter.im/AngularClass/angular2-webpack-starter
- */
