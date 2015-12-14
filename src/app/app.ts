491ac26d5b6b310c67994ae15b7aab22a4590094
Timothy Jones <palantar@gmail.com>
2015-12-13 18:28:39 -0800

    a working (though incredibly lame) implementation of saving one's own lunch suggestion to the group.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,13 @@
-/*
- * Angular 2 decorators and services
- */
 import {Directive, Component, ElementRef} from 'angular2/core';
 import {RouteConfig, Router} from 'angular2/router';
 import {Http, Headers} from 'angular2/http';
 import {FirebaseEventPipe} from './firebase/firebasepipe';
-import {NgIf} from 'angular2/common';
+import {NgIf, NgModel} from 'angular2/common';
 
-/*
- * Angular Directives
- */
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {Inject} from "angular2/core";
+
+declare var Materialize: any;
 
 
 /*
@@ -50,27 +47,34 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
   </div>
 
   <div class="container" *ngIf="isLoggedIn">
-    <br/>
-    <div>Add your suggestion or vote on other people's suggestions until 11:30.  At 11:30, voting stops and a spot is chosen!</div>
 
-    <h4>Add a Lunch Option</h4>
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <input placeholder="" id="title" type="text" class="validate">
-          <label for="title">Title</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input placeholder="" id="description" type="text" class="validate">
-          <label for="description">Description / Selling It</label>
-        </div>
-      </div>
-    </form>
+    <div>
+      <br/>
+      <div>Add your suggestion or vote on other people's suggestions until 11:30.  At 11:30, voting stops and a spot is chosen!</div>
 
-    <div class="right-align">
-      <a class="waves-effect waves-light btn"><i class="material-icons left">library_add</i>add</a>
+      <h4>Add a Lunch Option</h4>
+      <form class="col s12">
+        <div class="row">
+          <div class="input-field col s6">
+            <input placeholder="" id="title" type="text" class="validate" [(ngModel)]="mySuggestionTitle" required>
+            <label for="title">Title</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input placeholder="" id="description" type="text" class="validate" [(ngModel)]="mySuggestionDescription">
+            <label for="description">Description / Selling It</label>
+          </div>
+        </div>
+      </form>
+
+      <div class="right-align">
+        <a class="waves-effect waves-light btn" (click)="setMySuggestion(mySuggestionTitle,mySuggestionDescription)">
+          <i class="material-icons left">library_add</i>
+          <span *ngIf="hasSuggestion">Update</span>
+          <span *ngIf="!hasSuggestion">Add</span>
+        </a>
+      </div>
     </div>
 
     <h4>Rate other people's suggestions</h4>
@@ -118,10 +122,15 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 export class App {
   firebaseUrl: string;
   firebaseRef: Firebase;
+  userRef: Firebase;
+  mySuggestionRef: Firebase;
+  groupSuggestionsRef: Firebase;
   isLoggedIn: boolean;
   authData: any;
+  hasSuggestion: boolean;
+  mySuggestionTitle: String = "";
+  mySuggestionDescription: String = "BOOGERS";
   constructor() {
-
     this.firebaseUrl = "https://lunchtimevoter.firebaseio.com/";
     this.firebaseRef = new Firebase(this.firebaseUrl);
     this.firebaseRef.onAuth((user) => {
@@ -129,9 +138,47 @@ export class App {
         this.authData = user;
         this.isLoggedIn = true;
         console.log("user logged in.");
+
+        //Setup user tree.
+        this.userRef = new Firebase(this.firebaseUrl + "users/" + user.uid + "/");
+        this.userRef.set({provider: user.provider, group: "default"});
+
+        //Setup choice ref.
+        this.mySuggestionRef = new Firebase(this.firebaseUrl + "groups/default/choices/" + user.uid);
+
+        this.mySuggestionRef.on("value",this.onMySuggestionChanged.bind(this), function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+        });
+
+        //Setup Group Suggestions Ref
+        this.groupSuggestionsRef = new Firebase(this.firebaseUrl + "groups/default/choices/");
       }
     });
+  }
 
+  setMySuggestion(suggestionTitle,suggestionDescription){
+    if (this.mySuggestionRef && suggestionTitle ){
+      if (suggestionDescription){
+        this.mySuggestionRef.set({title: suggestionTitle, description: suggestionDescription});
+      }
+      else{
+        this.mySuggestionRef.set({title: suggestionTitle, description: ""});
+      }
+    }
+    Materialize.toast('Your suggestion has been saved.', 4000) // 4000 is the duration of the toast
+  }
+
+  onMySuggestionChanged(snapshot){
+    var myFirebaseSuggestion = snapshot.val();
+    if (myFirebaseSuggestion === undefined || myFirebaseSuggestion.title === undefined || myFirebaseSuggestion.title === ''){
+      this.hasSuggestion = false;
+    } else{
+      console.log('there');
+      this.hasSuggestion = true;
+      console.log("Changing internal vars to: " + JSON.stringify(myFirebaseSuggestion));
+      this.mySuggestionTitle = myFirebaseSuggestion.title;
+      this.mySuggestionDescription = myFirebaseSuggestion.description;
+    }
   }
 
   authenticateWithGoogle(){
@@ -147,5 +194,7 @@ export class App {
     this.firebaseRef.unauth();
     this.isLoggedIn = false;
   }
+
+
 }
 
